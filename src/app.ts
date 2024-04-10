94bb99d84c3ef61ea33af722e5ff17d7ea202cf6
Newton Duarte <newton_@hotmail.com>
2024-04-10 15:23:21 -0300

    feat: use controllers on fastify app

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,8 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
+import { register } from './http/controllers/register'
+import { listUsers } from './http/controllers/list-users'
 
 export const app = fastify()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return { users }
-})
-
-app.post('/users', async (request, reply) => {
-  const createUserSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(6),
-  })
-
-  const { name, email, password } = createUserSchema.parse(request.body)
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash: password,
-    },
-  })
-
-  return reply.status(201).send()
-})
+app.get('/users', listUsers)
+app.post('/users', register)
