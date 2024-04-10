c8addc586cccf2fcadb7965b89065d237f85243c
Newton Duarte <newton_@hotmail.com>
2024-04-10 16:14:36 -0300

    feat: catch user already exists error on register controller

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists-error'
 import { RegisterService } from '@/services/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
@@ -17,8 +18,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerService = new RegisterService(usersRepository)
 
     await registerService.execute({ name, email, password })
-  } catch {
-    return reply.status(409).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
