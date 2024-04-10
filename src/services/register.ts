a2f29c51cb70678f58c83f950c6968150a27d0bb
Newton Duarte <newton_@hotmail.com>
2024-04-10 16:08:10 -0300

    feat: use find user by email method on register service

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,4 +1,3 @@
-import { prisma } from '@/lib/prisma'
 import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
@@ -14,11 +13,7 @@ export class RegisterService {
   async execute({ name, email, password }: RegisterServiceRequest) {
     const passwordHash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('User with the same email already exist')
