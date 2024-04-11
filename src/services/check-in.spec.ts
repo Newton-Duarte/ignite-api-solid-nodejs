11b3581f97513829e01b1fe4d2812f74eb7f5532
Newton Duarte <newton_@hotmail.com>
2024-04-11 15:28:00 -0300

    test: add more check in unit tests

--- src/services/check-in.spec.ts
+++ src/services/check-in.spec.ts
@@ -19,8 +19,8 @@ describe('Check In service', () => {
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(0),
-      longitude: new Decimal(0),
+      latitude: new Decimal(-9.6542779),
+      longitude: new Decimal(-35.7079223),
     })
 
     vi.useFakeTimers()
@@ -81,5 +81,25 @@ describe('Check In service', () => {
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in on distant gym', async () => {
+    gymsRepository.gyms.push({
+      id: 'gym-02',
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: new Decimal(-27.0747279),
+      longitude: new Decimal(-49.4889672),
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-02',
+        userId: 'user-01',
+        userLatitude: -9.6542779,
+        userLongitude: -35.7079223,
+      }),
+    ).rejects.toBeInstanceOf(Error)
   })
 })
