d00d469077049428a18359aa675a8c1fbd2dd307
Newton Duarte <newton_@hotmail.com>
2024-04-11 20:55:36 -0300

    feat: add find many nearby method

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -4,8 +4,14 @@ export interface SearchManyParams {
   query: string
   page: number
 }
+
+export interface FindManyNearbyParams {
+  latitude: number
+  longitude: number
+}
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
   searchMany(params: SearchManyParams): Promise<Gym[]>
   create(data: Prisma.GymCreateInput): Promise<Gym>
 }
