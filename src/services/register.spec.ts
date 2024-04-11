ab8d8225fd22f4ff9b22df2e6f941bd58a26479b
Newton Duarte <newton_@hotmail.com>
2024-04-10 21:25:12 -0300

    test: add register unit tests

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,17 @@
-import { expect, test } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { expect, it, describe } from 'vitest'
+import { RegisterService } from './register'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register service', () => {
+  it('should be able to create an user', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+    const { user } = await registerService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
 })
