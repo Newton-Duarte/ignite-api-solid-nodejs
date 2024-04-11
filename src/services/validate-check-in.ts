d7db54738b87a5b9344d52a7135a1d2fd3c3da37
Newton Duarte <newton_@hotmail.com>
2024-04-11 20:58:37 -0300

    feat: add validate check in service

---
+++ src/services/validate-check-in.ts
@@ -0,0 +1,33 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { ResourceNotFoundError } from '@/services/errors/resource-not-found-error'
+import { CheckIn } from '@prisma/client'
+
+interface ValidateCheckInServiceRequest {
+  checkInId: string
+}
+
+interface ValidateCheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class ValidateCheckInService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    checkInId,
+  }: ValidateCheckInServiceRequest): Promise<ValidateCheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.findById(checkInId)
+
+    if (!checkIn) {
+      throw new ResourceNotFoundError()
+    }
+
+    checkIn.validated_at = new Date()
+
+    await this.checkInsRepository.save(checkIn)
+
+    return {
+      checkIn,
+    }
+  }
+}
