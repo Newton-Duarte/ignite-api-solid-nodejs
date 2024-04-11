81ca51d1d7b52a80e4f52ad4e342b189a51cc346
Newton Duarte <newton_@hotmail.com>
2024-04-11 20:53:04 -0300

    feat: add search many method

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -1,6 +1,11 @@
 import { Gym, Prisma } from '@prisma/client'
 
+export interface SearchManyParams {
+  query: string
+  page: number
+}
 export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
+  searchMany(params: SearchManyParams): Promise<Gym[]>
   create(data: Prisma.GymCreateInput): Promise<Gym>
 }
