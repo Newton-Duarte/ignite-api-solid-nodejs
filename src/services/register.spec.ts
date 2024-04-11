db53e36b348d9a5ec89f1d6c9d37601945a51888
Newton Duarte <newton_@hotmail.com>
2024-04-10 21:27:03 -0300

    test: add register user with same email unit tests

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { expect, it, describe } from 'vitest'
 import { RegisterService } from './register'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register service', () => {
   it('should be able to create an user', async () => {
@@ -13,5 +14,23 @@ describe('Register service', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to create an user with the same email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+    await registerService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    await expect(() =>
+      registerService.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
