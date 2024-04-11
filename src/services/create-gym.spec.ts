1a1e4d88b5c6681dd350d132952afdee48881c2b
Newton Duarte <newton_@hotmail.com>
2024-04-11 15:41:10 -0300

    test: add create gym unit tests

---
+++ src/services/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { expect, it, describe, beforeEach } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymService } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymService
+
+describe('Create Gym service', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymService(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: '',
+      phone: '',
+      latitude: -9.6542779,
+      longitude: -35.7079223,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
