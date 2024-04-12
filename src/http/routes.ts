c9924b510d31921f28d497e6cd1b8203f04dbf92
Newton Duarte <newton_@hotmail.com>
2024-04-12 10:22:43 -0300

    feat: add profile controller with get me route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,14 @@ import { FastifyInstance } from 'fastify'
 import { listUsers } from './controllers/list-users'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.get('/users', listUsers)
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated
+
+  app.get('/me', profile)
 }
