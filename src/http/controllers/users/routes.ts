f6115192da0cd7e7ae2d6a3ed0e319d011b3c69a
Newton Duarte <newton_@hotmail.com>
2024-04-13 18:42:24 -0300

    feat: add refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/users', listUsers)
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refresh)
 
   // Authenticated
   app.get('/me', { onRequest: [verifyJwt] }, profile)
